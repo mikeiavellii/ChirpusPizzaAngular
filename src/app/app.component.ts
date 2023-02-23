306f36c43390a4c07d3ac17a8668e8510684ce29
Mike <mike.blackburn.541@gmail.com>
2023-02-23 15:18:35 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChirpusPizzaLab';
+
+  MenuItems:MenuItem[] = [
+    {
+      item: "Pizza",
+      category: "Pizza",
+      price: 5.00
+    },
+    {
+      item: "Pepsi",
+      category: "Drinks",
+      price: 1.00
+    },
+    {
+      item: "Bread Stick",
+      category: "Pizza",
+      price: 3.00
+    },
+    {
+      item: "Wings",
+      category: "Sides/Apps",
+      price: 3.00
+    },
+    {
+      item: "Tacos",
+      category: "Sides/Apps",
+      price: 2.00
+    },
+    {
+      item: "Water",
+      category: "Drinks",
+      price: 1.00
+    },
+    {
+      item: "Jalapeno Poppers",
+      category: "Sides/Apps",
+      price: 2.50
+    },
+];
 }
